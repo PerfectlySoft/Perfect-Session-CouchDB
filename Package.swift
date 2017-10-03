8a5568b8b51d1879b547a463ce8b926bf875006c
Jonathan Guthrie <jono@perfect.org>
2017-10-03 17:04:26 -0400

    update for swift 4 perfect 3

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-12 20:32:37 +0000
+// Date: 2017-10-03 20:57:15 +0000
 import PackageDescription
 let package = Package(
-    name: "PerfectSessionCouchDB",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 1),
-        .Package(url: "https://github.com/SwiftORM/CouchDB-StORM.git", majorVersion: 1),
-    ]
+	name: "PerfectSessionCouchDB",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 3),
+		.Package(url: "https://github.com/SwiftORM/CouchDB-StORM.git", majorVersion: 3),
+	]
 )
